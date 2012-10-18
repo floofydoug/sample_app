8783f40c06bdf3ca3500899958dd19b2d09ed4de
Douglas Phung <Doug@chillportal.com>
2012-10-17 21:30:51 -0400

    initial sample commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e187b42350fdc8f6e51ee8fd3eedec2e67f1717c8cb39dc71d43520114174a90f8985212ddeb032b39ce5fc178ea121ead07c8d81eb1771aaa7b744834c92ae7'
